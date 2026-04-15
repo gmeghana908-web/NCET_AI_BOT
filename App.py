ddb71b1746de6bf647f5491de8637c23c72029e1
gmeghana908-web <gmeghana908@gmail.com>
2026-04-15 15:33:27 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from transformers import pipeline
+
 # Load the summarization model
 @st.cache_resource
 def load_summarizer():
@@ -12,11 +13,11 @@ st.title("🤖 AI Text Summarizer")
 st.write("Enter a long text below, and get a concise summary!")
 
 #Text Input
-long_text = st.text_area("Enter text to summarize:",height=200)
+long text = st.text_area("Enter text to summarize:",height=200)
 
 #Summary Parameters
-max_length = st.slider("Max Summary Length", min_value = 50, max_value = 300, value = 130)
-min_length = st.slider("Min Summary Length", min_value = 20, max_value = 100, value = 38)
+max length = st.slider("Max Summary Length", min_value = 50, max_value = 300, value = 130)
+min length st.slider("Min Summary Length", min_value = 20, max value = 100, value = 38)
 
 if st.button("Summarize"):
     if long_text.strip():
@@ -26,5 +27,3 @@ if st.button("Summarize"):
         st.success(summary[0]['summary_text'])
     else:
         st.warning("⚠️ Please enter some text to summarize.")
-   
-    
